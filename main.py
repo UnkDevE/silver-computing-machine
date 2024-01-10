07318501d20e83f8df6cda43414f585bcf489afe
Ethan Riley <riley787cmplex@gmail.com>
2024-01-10 00:20:43 +0000

    enough for today

--- main.py
+++ main.py
@@ -1,207 +1,108 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import scipy as sp
 import sympy as syp
+import sympy
 
 import sys
-
-"""
-script start
-
-General Outline
-Nerual nets are function approximators
-We can extract the function the neural net is approximating
-
-Math statements
-
-A matrix is transformation of a vector A matrix of directions transforming a vector is a function
-This works in any dimensions of matrix and vector
-if a vector is an input space N then a matrix of NxN is a eigenfunction that defines it
-if a function is one to one it corresponds to these matricies
-if the determinant of a matrix is 0 then the matrix reduces by at least 1 dimension
-All continous functions can be writtern as a polynomial
-
-Comp sci statments
-Neural networks are an approximation of fitting the curve and finding it's minima
-for a function that we are unable to define programmatically.
-
-Feedfoward is:
-Each activation is the dot product of weights and baises in respect to the previous layer 
-put the nonlinearity function which acts as renormilization into the boundaries 0 and 1.
-
-Backpropagation is:
-We take the average of each training sample 
-Then we take the derivative of the cost function over the weights:
-(which is the chain rule of z/w * a/z * C/a)
-We then apply our nonlinearity function
-
-OUR WORKING OUT:
-Idea:
-We should be able to extract the functions of the nerual network mathematically
-
-lemma:
-any non linear transform is linear in a higher dimension.
-proof: Hahn–Banach theorem.
-
-explaination:
-given a function F_1 and it's finite transform M,
-so long as the non linear transform is continous,
-in a higher dimension the transform is linear.
-
----
-Idea: to exchange notation of a finite vector transform into a function
-
-prop:
-lets do this sandwitch method, 
-we have a function F(x) it's corresponding vector would be uncountably infinite.
-we have a finite vector V that corresponds to a function.
-how can we approximate function F(x)?
-
-lemma: 
-spaces inbetween vector V are uncountably infinite and continous.
-
-prop:
-rearrange Feedfoward into product polynomial format:
-https://en.wikipedia.org/wiki/Recurrence_relation#Linear_homogeneous_recurrence_relations_with_constant_coefficients
-https://en.wikipedia.org/wiki/Linear_recurrence_with_constant_coefficients#Solution_using_generating_functions
-track variables there with products
-
-PROBLEM: activation function - relu and such uses 
-how can we take this into our function
-
-idea: the activation function cancels terms by creating a signularity
-this is most notable with relu
-
-sol: we cancel out previous terms that the activation function cancels out, and keep a record of them
-so we when we Feedfoward things will be fine
-prop:
-we apply the same matrix of weights (function on the basis) to a new starting point(training batches),
-and widen the search net of the training samples until the whole sample space is covered
-
-idea: backpropagation is a specific case of convolution
-
-prop: 
-as the neural network is like function approximator 
-it's matrix is like a conversion table between the input and output
-each layer a coefficient in an unknown polynomial F(x).
-
-lem: Backpropagation is convolution of two polynomials with the same coefficients.
-
-idea: polynomial approaches a taylor series, we can recover a function from this 
-lem: use the 
-
-prop: 
-smooth matrix polynomials which have finite terms
-an infinite polynomial will be able to be recovered with a transform
-
-proof: https://www.youtube.com/watch?v=zvbdoSeGAgI
-
-Idea: Now make it within bounds
-
-prop:
-change the bounds of neural networks from 0 to 1 this is already true.
-if the bounds are this we can use foruier transform.
-which is an inverse laplace
-
-qed: inverse laplace transforms recover the function of a neural network.
-
-
-Idea: mutlidim inverse fourier transforms
-as all of the weights are on the real number line
-prop: product sum each weight vector as inputs along each layer.
-
-ideas:
-the universe is a closed space on a higher dim, a number line must be the same.
-trajectoids -> a to find seperate functions in the space it took not the most efficient way of find the function but the actual path the nerual network took <- BIG LEAD
-infinite weights => function local minima
-BIG UPDATE - polynomial is a cauchy sequence.
-matrix exponents
-divergence and curl for functions => Discrete matrix transforms
-borsuk ulam theorem?
-e^Mt = transform / rotation
-meta-networks => deriving the function from networks and then inverse fourier 
-to create genralized function. Same idea as finding the transform for training batches.
-IS FOURIER TRANSFORM A RED HERRING?
-eigenstates for input matricies
-foruier transforms => to create human readable functions
-quanta + tunneling to find absoloute minima =>
-
-minibatch selectors => HYP: 
-taking the largest distance inputs then getting smaller distances 
-until finding convergence 
-Once convergence is found for local minima i.e. det == 0 
-
-find if convergable first?
-
-"""
+import os
+import inspect
 
 # setup symbols for computation
 weight= syp.Symbol("w_0")
 activation = syp.Symbol("a")
 bias = syp.Symbol("b_0")
+output = syp.Symbol("y")
 activation_fn = syp.Function("sigma")
 
 def get_poly_eqs(layers_total):
     # create neruonal activation equations
-    staring_eq = syp.Eq(activation_fn((weight * activation) + bias))
+    staring_eq = activation_fn((weight * activation) + bias)
     eq_system = [staring_eq]
 
     # summate equations to get output
     for i in range(1, layers_total):
-        eq_system.append(syp.Eq(activation_fn(syp.Symbol("w_" + str(i)) * eq_system[-1]) + bias))
+        eq_system.append(activation_fn(syp.Symbol("w_" + str(i)) * eq_system[-1]) + bias)
 
     return eq_system
 
 
-def subst_into_system(fft_layers, eq_system, def_activ_fn):
-    for i in range(0, fft_layers.len()):
-        weight_symbol = syp.Symbol("w_" + str(i))
-        bias_symbol = syp.Symbol("b_" + str(i))
+def activation_fn_lookup(activ_obj):
+    obj = inspect(getsource(activ_obj))
+    expression = sympy.sympify(ast.unparse(obj).)
 
-        # fft_layers[n][0] is weights whilst 1 is baises
-        eq_system.subst(weight_symbol, fft_layers[i][0])
-        eq_system.subst(bias_symbol, fft_layers[i][1])
-        eq_system.subst(activation_fn, def_activ_fn)
+    return expression
+
+def subst_into_system(fft_layers, eq_system):
+    # skip input
+    for i in range(0, len(fft_layers)):
+        for wb in range(0, len(fft_layers[i][0])):
+            print(fft_layers[i][0][wb])
+            weight_symbol = syp.Symbol("w_" + str(wb))
+            bias_symbol = syp.Symbol("b_" + str(wb))
+
+            for system in eq_system:
+                # fft_layers[n][0] is weights whilst 1 is baises
+                system.subs(weight_symbol, fft_layers[i][0][0][0][wb])
+                system.subs(bias_symbol, fft_layers[i][0][1][wb])
+                system.subs(activation_fn, activation_fn_lookup(fft_layers[i][1]))
     
     return np.array(eq_system)
 
 # evaluate irfftn using cauchy residue theorem
-def evaluate_system(eq_system):
-   feedfoward_system = eq_system.sum()
+def evaluate_system(eq_system, fft_inverse, tex_save):
+   feedfoward_system = eq_system[len(eq_system) - 1] # last eq is the output
    result = None
-   from sympy import inverse_fourier_transform 
-   try:
-       result = inverse_fourier_transform(feedfoward_system, weight, bias).doit()
-   except IntergralTransformError:
-       print("could not be evaluated switching to scipy")
+   # inverse of fourier transform is anaglogous to convergence of fourier series
+   from sympy import fourier_series, solve, latex
+   series = fourier_series(feedfoward_system, limits=(weight, 0, 1), finite=True).doit(deep=True)
    print("eq evaluated")
-   from sympy import latex
-   latex(result)
+
+   file = open(tex_save, "xt")
+   file.write(latex(solve(result)))
+   file.close()
+
+   print("eq solved")
    return result
 
-def model_create_equation(model_dir):
+def model_create_equation(model_dir, tex_save):
     # create prequesties
     model = tf.keras.models.load_model(model_dir)
-    if model not None:
-        peq_system = get_poly_eqs(model.layers.len())
+    if model != None:
+        peq_system = get_poly_eqs(len(model.layers))
         
         # calculate fft
         from scipy import fft as fft
-        from scipy import laplace as lap
-        layers = []
-        for layer in model.layers:
+        from keras import backend as K
+        from sympy import parse_expr
+        fft_layers = []
+        nparr = []
+        for [wb_layer, act] in [
+                ([layer.weights[0], layer.weights[1]], layer.activation)
+                          for layer in model.layers if len(layer.weights) > 1]:
             # fft for interpolation or finding the polynomial
             # fft == inverse laplace.
-            w_layer = layer.get_weights()[0]
-            b_layer = layer.get_weights()[1]
-            
-            layer = (w_layer, b_layer)
+            nparr.append(wb_layer)
+            if act != None:
+                fft_layers.append([wb_layer, act])
+            else: 
+                fft_layers.append([wb_layer, parse_expr("x=x")])
+
         peq_system = subst_into_system(fft_layers, peq_system)
-        result = evaluate_system(eq_system)
+        fft_inverse = fft.irfftn(np.array(nparr))
+        result = evaluate_system(peq_system, fft_inverse, tex_save)
 
 if __name__=="__main__":
-    try:
-        model_create_equation(argv[2])
-    except:
-        print("please provide model dir as command line argument")
+    if len(sys.argv) > 2:
+        path = os.path.dirname(__file__)
+        try:
+            model = os.path.join(path, sys.argv[1])
+            print(str(model))
+            model_create_equation(os.path.abspath(model), sys.argv[2])
+        except FileNotFoundError:
+            print("""file not found, 
+please give filename of model to extract equation from""")
+    else:
+        print("""not enough commands, please give a filename of a model to extract, 
+and output for a tex file""")
+
