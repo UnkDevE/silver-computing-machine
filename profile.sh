7ab3dd3967195c308ea65c40092508e884d5f2d0
Ethan Riley <riley787cmplex@gmail.com>
2026-02-20 23:03:01 +0000

    needs profilin

--- profile.sh
+++ profile.sh
@@ -1,3 +1,6 @@
 #/bin/sh
+ulimit -n 2048
 source ./.venv/bin/activate
-py-spy record python main.py 225 1 vgg11 IMAGENET1K_V1 CrossEntropyLoss Adam
+echo "reproducable seed (set to 0 if new test)"
+read seed
+HIP_VISIBLE_DEVICES=0 python main.py 225 1 vgg11 IMAGENET1K_V1 CrossEntropyLoss Adam 0 $seed 
